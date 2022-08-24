feeccaca5fdcbfe48ffecb86871497cfb10212aa
ArielBetti <ariel.betti@gmail.com>
2022-08-24 01:13:30 -0300

    feat(user-model): add avatar in user model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,28 @@
 const mongoose = require("../database");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      require: true,
+const userSchema = new mongoose.Schema({
+  avatar: {
+    type: {
+      accessoires: [String],
+      backgroundColor: [String],
+      eyes: [String],
+      eyebrows: [String],
+      mouth: [String],
+      flip: Boolean,
+      url: String,
     },
-    password: {
-      type: String,
-      require: true,
-      select: false,
-    },
+    require: true,
   },
-);
+  name: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    select: false,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 
