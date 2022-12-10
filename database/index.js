83361aee3a20ec17dcea3fe62aafeb751d98b97c
ArielBetti <ariel.betti@gmail.com>
2022-12-10 12:25:04 -0300

    fix: data base string env

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  `mongodb+srv://watchthis:${process.env.DATA_BASE_PASSWORD}@watch-this-cluster.uufoq35.mongodb.net/watch-this?retryWrites=true&w=majority`,
+  process.env.DATA_BASE_CONNECTION_STRING,
   {
     useUnifiedTopology: true,
     useNewUrlParser: true,
